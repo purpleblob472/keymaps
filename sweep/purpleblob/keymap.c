0698d013f4ee9838e36040c13c338443cf73a55b
Daniel <daniel@localhost.localdomain>
2024-05-25 23:33:01 +0100

    added mod tap

--- sweep/purpleblob/keymap.c
+++ sweep/purpleblob/keymap.c
@@ -60,10 +60,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //Main Layer
     [0] = LAYOUT(
 
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_BSPC,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_ENT,
-                                   KC_LSFT, MO(1), LT(2, KC_SPC),  KC_LCTL
+        KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
+        KC_P,         KC_Z,         KC_X,         KC_C,         KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,
+                                   KC_BSPC, MO(1), LT(2, KC_SPC),  KC_LCTL
     ),
 
 //Punc Layer
@@ -71,7 +71,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_TAB,  KC_AT,   KC_LCBR, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_RCBR, KC_QUOT, KC_SCLN,
-        KC_DQT,  KC_NUHS, KC_PERC, KC_CIRC, KC_LALT, KC_LGUI, KC_DLR,  KC_ASTR, KC_NUBS, KC_SLSH,
+        KC_DQT,  KC_NUHS, KC_PERC, KC_COLN, _______, KC_CIRC, KC_DLR,  KC_ASTR, KC_NUBS, KC_SLSH,
                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         KC_ESC,  KC_EXLM, S(KC_NUBS), KC_AMPR, _______, KC_INS,  TD(TF1), TD(TF5), TD(TF9), KC_DEL,
         KC_LEFT, KC_DOWN, KC_UP,      KC_RGHT, _______, _______, C(KC_LEFT), C(KC_DOWN), C(KC_UP), C(KC_RGHT),
-        S(KC_NUHS), KC_PGDN, KC_PGUP,    KC_HOME, _______, _______, KC_END,  _______, _______, _______,
+        S(KC_NUHS), KC_PGDN, KC_PGUP, KC_HOME, _______, _______, KC_END,  _______, _______, _______,
                                       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
