5b84bafce45def4be7491f403081220de29cd981
Daniel <daniel@localhost.localdomain>
2024-05-26 16:24:09 +0100

    comfort changes

--- sweep/purpleblob/keymap.c
+++ sweep/purpleblob/keymap.c
@@ -61,9 +61,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
 
         KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,
+        LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    LSFT_T(KC_ENT), 
         KC_P,         KC_Z,         KC_X,         KC_C,         KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,
-                                   KC_BSPC, MO(1), LT(2, KC_SPC),  KC_LCTL
+                                    MO(1),      KC_BSPC, LT(2, KC_SPC),  KC_LCTL
     ),
 
 //Punc Layer
@@ -88,34 +88,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [3] = LAYOUT(
 
         TD(TH1), TD(TH2), TD(TH3), TD(TH4), TD(TH5), TD(TH6), TD(TH7), TD(TH8), TD(TH9), TD(TH0),
-        A(KC_SPC),_______,_______, _______, _______, _______, _______, _______, _______, _______,
-        KC_CAPS, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        A(KC_SPC),_______,_______, _______, _______, _______, KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT,
+        KC_CAPS, _______, _______, _______, CM_TOGG, DF(4),   _______, _______, _______, CW_TOGG,
                                    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
 //Gaming Layer
     [4] = LAYOUT(
 
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    _______, _______, _______, _______, _______,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    _______, _______, _______, _______, _______,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    _______, _______, _______, _______, _______,
+        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_BSPC,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT, 
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,
                                    MO(5), KC_SPC,  _______, _______
     ),
 
 //Gaming Upper Layer
     [5] = LAYOUT(
 
-        KC_GRV, KC_1,    KC_2,    KC_3,    KC_4,    _______, _______, _______, _______, _______,
-        KC_TAB, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______, _______, _______,
-        KC_LALT, KC_I,    KC_P,    KC_G,    KC_M,    _______, _______, _______, _______, _______,
-                                  _______, _______, _______, _______
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_TAB,  KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,  KC_RBRC, KC_RPRN, KC_RCBR, KC_QUOT, KC_SCLN,
+        KC_LALT, KC_T, KC_G, KC_V, _______, KC_CIRC, KC_DLR,  KC_ASTR, KC_NUBS, KC_SLSH,
+                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
 };
 
-const uint16_t PROGMEM cmb_mins[] = {KC_D, KC_F, COMBO_END};
+const uint16_t PROGMEM cmb_mins[] = {LALT_T(KC_D), LGUI_T(KC_F), COMBO_END};
 const uint16_t PROGMEM cmb_plus[] = {KC_J, KC_K, COMBO_END};
-const uint16_t PROGMEM cmb_eql[] = {KC_F, KC_J, COMBO_END};
+const uint16_t PROGMEM cmb_eql[] = {LGUI_T(KC_F), KC_J, COMBO_END};
 const uint16_t PROGMEM cmb_unds[] = {KC_C, KC_N, COMBO_END};
 combo_t key_combos[] = {
     COMBO(cmb_mins, KC_MINS),
