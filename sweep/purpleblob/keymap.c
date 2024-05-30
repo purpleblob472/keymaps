13ef7650797d0ed32c61c4149e3dfb24aa11e5f0
daniel <daniel.steven.2022@gmail.com>
2024-05-30 17:23:25 +0100

    fixed combos

--- sweep/purpleblob/keymap.c
+++ sweep/purpleblob/keymap.c
@@ -64,7 +64,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
 
         KC_Q,         KC_W,         KC_E,         KC_R,        KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI(KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_ENT), 
+        LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G, KC_H, KC_J, KC_K, KC_L, LSFT_T(KC_ENT), 
         KC_SLSH, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M,    KC_COMM, KC_DOT,
                         KC_BSPC, MO(1), LT(2, KC_SPC),  LCTL_T(KC_SCLN)
     ),
@@ -74,8 +74,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_TAB,  KC_AT,   KC_LCBR, KC_LPRN, KC_LBRC, KC_RBRC, KC_RPRN, KC_RCBR, KC_QUOT, KC_DQT, 
-        KC_NUHS,  KC_EXLM, S(KC_NUBS), KC_AMPR, KC_PERC, KC_CIRC, KC_DLR,  KC_ASTR, KC_NUBS, KC_SLSH,
-                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_NUHS,  KC_EXLM, S(KC_NUBS), KC_AMPR, KC_PERC, KC_CIRC, KC_DLR,  KC_ASTR, KC_LT, KC_GT,
+                                   KC_TRNS, KC_TRNS, KC_TRNS, KC_COLN
     ),
 
 //Nav Layer
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //KC_AT (KC_DQT), KC_INS, KC_DEL
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
         KC_LEFT, KC_DOWN,    KC_UP,   KC_RGHT, KC_F11, KC_F12, C(KC_LEFT), C(KC_DOWN), C(KC_UP), C(KC_RGHT),
-        S(KC_NUHS), KC_PGDN, KC_PGUP, KC_HOME, KC_INS, KC_DEL, KC_END,  _______, _______, _______,
+        S(KC_NUHS), KC_PGDN, KC_PGUP, KC_HOME, KC_INS, KC_DEL, KC_END,  _______, _______, KC_NUBS,
                                       KC_ESC, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
